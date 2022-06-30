e6a41a84829f1186240d027e0bd8d6079c1a9a7a
yaser <ddemety372@gmail.com>
2022-06-30 22:58:22 +0300

    fetch

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,30 @@
-
-
+import { useState } from "react";
 const Users = () => {
+  const [users, setUsers] = useState();
+
+  const getUsers = () => {
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+   .then((data) => setUsers(data));
+
+  }
+
+//   getUsers();
+//    console.log(users);
+
   return (
     <div>
-        
+      <h1>User List</h1>
+      {/* <button onClick={getUsers}>GET USER</button> */}
+      <div className="row">
+        <div className="col">
+          <img src="" alt="" />
+          <h6></h6>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
